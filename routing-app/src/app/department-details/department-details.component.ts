779064d5b1a52fe788ab73e5b00a644bf04dc88c
Gattuoch Kuon <kuon2018@outlook.com>
2020-06-23 09:35:02 -0600

    Added detail component

---
+++ routing-app/src/app/department-details/department-details.component.ts
@@ -0,0 +1,20 @@
+import { Component, OnInit } from '@angular/core';
+
+@Component({
+  selector: 'app-department-details',
+  template: `
+    <p>
+      department-details works!
+    </p>
+  `,
+  styles: [
+  ]
+})
+export class DepartmentDetailsComponent implements OnInit {
+
+  constructor() { }
+
+  ngOnInit(): void {
+  }
+
+}
