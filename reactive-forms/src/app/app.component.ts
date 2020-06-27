6a158c25d09fc4af66c1736172d404b5c0c9710a
Gattuoch Kuon <kuon2018@outlook.com>
2020-06-27 16:19:34 -0600

    Added more reactive form functionality

--- reactive-forms/src/app/app.component.ts
+++ reactive-forms/src/app/app.component.ts
@@ -1,4 +1,5 @@
 import { Component } from '@angular/core';
+import { FormGroup, FormControl } from '@angular/forms';
 
 @Component({
   selector: 'app-root',
@@ -6,5 +7,28 @@ import { Component } from '@angular/core';
   styleUrls: ['./app.component.css']
 })
 export class AppComponent {
-  title = 'reactive-forms';
+  registrationForm = new FormGroup({
+    userName: new FormControl('Ban-Gatto'),
+    password: new FormControl(''),
+    confirmPassword: new FormControl(''),
+    Address:new FormGroup({
+      state: new FormControl(''),
+      city: new FormControl(''),
+      postalCode: new FormControl('')
+    })
+  })
+
+  loadApiData(){
+    this.registrationForm.setValue({
+      userName:'Kuon',
+      password: 'tester',
+      confirmPassword: 'tester',
+      Address: {
+        city:'Montreal',
+        state: 'Quebec',
+        postalCode: 'H2X 2K9'
+      }
+    })
+    
+  }
 }
